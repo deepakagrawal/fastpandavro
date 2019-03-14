5fa2a4e8b80751aa8bfab82c9d7cf681cc063b73
DEEPAK AGRAWAL <deepak.agrawal@aa.com>
2019-03-14 10:36:03 -0500

    Creating new repository
    Signed-off-by: 099391 <deepak.agrawal@aa.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='FastAvroToPandas',
+      version='0.1',
+      description='Parallel conversion of Avro file to Pandas dataframe',
+      url='http://github.com/storborg/funniest',
+      author='Deepak Agrawal',
+      author_email=['agrawal.deepankur@gmail.com','deepak.agrawal@aa.com'],
+      license='MIT',
+      packages=['AvroToPandas'],
+      zip_safe=False)
