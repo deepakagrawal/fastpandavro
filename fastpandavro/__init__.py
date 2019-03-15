a995f036281676256d1119c9432051cf5b58b8d0
Deepak Agrawal <agrawal.deepak@outlook.com>
2019-03-14 23:05:45 -0500

    Update __init__.py

--- fastpandavro/__init__.py
+++ fastpandavro/__init__.py
@@ -6,17 +6,19 @@ from fastavro import writer, block_reader, parse_schema
 from joblib import Parallel, delayed
 from pandas import DataFrame
 
-process_block = lambda i: [record for record in i]
+
+def process_block(i):
+    return [record for record in i]
+
 
 def avro_to_pandas(fname, reader_schema=None, num_cores=None):
-    '''
-    Converts Avro file to pandas dataframe using parallel processing.
+    """    Converts Avro file to pandas dataframe using parallel processing.
     :param fname: path of avro file to be converted
     :param reader_schema: if schema of avro file is available (optional)
     :param num_cores: Number of processors to use (optional). By default uses all processors available
     :return: Pandas dataframe
-    '''
-    if num_cores==None:
+    """
+    if num_cores is None:
         num_cores = cpu_count()
     with open(fname, 'rb') as fo:
         avro_reader = block_reader(fo, reader_schema)
@@ -26,13 +28,13 @@ def avro_to_pandas(fname, reader_schema=None, num_cores=None):
 
 
 def pandas_to_avro(df, fname, schema_file):
-    '''
+    """
     Converts pandas dataframe to avro file
     :param df: Pandas dataframe
     :param fname: path where avro file will be saved
-    :param reader_schema: schema of the avro file
+    :param schema_file: schema of the avro file
     :return: Nothing to return
-    '''
+    """
     with open(schema_file) as schema:
         reader_schema = json.load(schema)
     with open(fname, 'wb') as out:
