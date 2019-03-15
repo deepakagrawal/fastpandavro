0edc1fe4081cbc792d636c333f3072f0c85c68ba
Deepak Agrawal <agrawal.deepak@outlook.com>
2019-03-14 22:30:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='fastpandavro',
       version='0.1',
       description='Parallel conversion of Avro file to Pandas dataframe and conversion of pandas dataframe to avro file',
-      url='https://ghe.aa.com/OR/AvroToPandas',
+      url='https://github.com/deepakagrawal/fastpandavro',
       author='Deepak Agrawal',
       author_email=['agrawal.deepankur@gmail.com','deepak.agrawal@aa.com'],
       license='MIT',
